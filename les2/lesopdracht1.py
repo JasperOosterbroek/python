2ee8e3c3b9e41eadf5cdfae2a345eda127c19c5e
jasper oosterbroek <jasper.oosterbroek@student.hu.nl>
2017-09-08 15:30:17 +0200

    Add les 2

---
+++ les2/lesopdracht1.py
@@ -0,0 +1,6 @@
+line1 = 'Hello Python developer...'
+line2 = 'Welcome to the world of Python!'
+print(line1)
+print(line2)
+
+name = input('Enter your name: ')
---
+++ les2/pe2_2.py
@@ -0,0 +1,7 @@
+cijferICOR = 6
+cijferPROG = 8
+cijferCSN = 7
+gemiddelde = (cijferICOR + cijferPROG +  cijferCSN) / 3
+beloning = (cijferICOR + cijferPROG +  cijferCSN) * 30
+print(type(gemiddelde))
+print('Mijn cijfers (gemiddeld een ' + str(gemiddelde) + ') leveren een belonging van '+str(beloning)+ ' op!')
